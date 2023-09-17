259cec6cfb051269342e71bf06141a5a5dff1ff9
Point <wuyuhuiss@outlook.com>
2023-09-17 15:18:07 +0800

    feat: test and project init completed

--- src/main/java/com/point/pointcodesandbox/controller/MainController.java
+++ src/main/java/com/point/pointcodesandbox/controller/MainController.java
@@ -1,2 +1,12 @@
-package com.point.pointcodesandbox.controller;public class MainController {
+package com.point.pointcodesandbox.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MainController {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "ok";
+    }
 }

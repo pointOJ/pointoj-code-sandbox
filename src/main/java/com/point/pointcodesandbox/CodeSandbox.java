d9276c05c1d97cbb8b2ca24c88271e210174199e
Point <wuyuhuiss@outlook.com>
2023-09-21 21:15:19 +0800

    feat: add java native code sandbox

---
+++ src/main/java/com/point/pointcodesandbox/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.point.pointcodesandbox;
+
+
+import com.point.pointcodesandbox.model.ExecuteCodeRequest;
+import com.point.pointcodesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
